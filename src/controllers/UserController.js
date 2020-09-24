85ca6c0653b9d92b44907b8ad8326e60349c67ac
Marcos Krul <marcoskrul2k@gmail.com>
2020-09-24 15:19:43 -0300

    crud - controllers

---
+++ src/controllers/UserController.js
@@ -0,0 +1,42 @@
+const knex = require("../database")
+
+module.exports = {
+    async index(req, res)  {
+        const results = await knex('users')
+        return res.json(results)
+    },
+    async create(req, res, next) {
+        try {
+            const { user_name } = req.body
+            await knex('users').insert({
+                user_name
+            })
+            return res.status(201).send()
+        } catch (error) {
+            next(error)
+        }
+    },
+    async update(req, res, next) {
+        try {
+            const { id } = req.params
+            const { user_name } = req.body
+            await knex('users')
+                .update( {user_name} )
+                .where( {id} )
+            return res.status(200).send()
+        } catch (error) {
+            next(error)
+        }
+    },
+    async delete(req, res, next) {
+        try {
+            const { id } = req.params
+            await knex('users')
+                .where( {id} )
+                .del()
+            return res.status(200).send()
+        } catch (error) {
+            next(error)
+        }
+    }
+}

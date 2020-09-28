f1820e848f487bdef8504c650197689a1e8a47f9
Marcos Krul <marcoskrul2k@gmail.com>
2020-09-28 18:50:36 -0300

    Routes for projects

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,18 @@ const routes = express.Router()
 
 const UserController = require('./controllers/UserController')
 const EmailController = require('./controllers/EmailController')
+const ProjectController = require('./controllers/ProjectsController')
 
 routes.get('/users', UserController.index)
 routes.post('/users', UserController.create)
 routes.put('/users/:id', UserController.update)
 routes.delete('/users/:id', UserController.delete)
+
 routes.get('/users/send/:name', EmailController.sendEmail)
+
+routes.get('/projects', ProjectController.index)
+routes.post('/projects', ProjectController.create)
+routes.put('/projects/:id', ProjectController.update)
+routes.delete('/projects/:id', ProjectController.delete)
 
 module.exports = routes
